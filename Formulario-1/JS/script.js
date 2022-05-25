0bc50aac303351133a8eef0bdc94988ebe8594ab
Pietra Valentina <pievalentinah@gmail.com>
2022-05-25 17:14:50 -0300

    commit initial

---
+++ Formulario-1/JS/script.js
@@ -0,0 +1,17 @@
+ var email = document.getElementById('email');
+
+ email.addEventListener('focus', ()=>{
+     email.style.borderColor = "rgb(117 44 197)"});
+
+ email.addEventListener('blur', ()=>{
+     email.style.borderColor = "rgb(117 44 197)"
+ });
+
+ var password = document.getElementById('password');
+
+ password.addEventListener('focus', ()=>{
+    password.style.borderColor = "rgb(117 44 197)"});
+
+password.addEventListener('blur', ()=>{
+    password.style.borderColor = "rgb(117 44 197)"
+});
